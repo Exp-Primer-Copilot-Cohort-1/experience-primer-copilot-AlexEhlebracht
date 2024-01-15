6bb4d21266253b54771308082331cf9f4e85b753
Alex <54728785+AlexEhlebracht@users.noreply.github.com>
2024-01-15 02:53:41 +0000

    do this

--- comments.js
+++ comments.js
@@ -1,73 +1,31 @@
 // create web server
+// start server: node comments.js
+// test: http://localhost:8080/comments
 
-// load modules
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var http = require('http');
+var url = require('url');
+var items = [];
 
-// load models
-var Comments = require('../models/comments');
+var server = http.createServer(function(req, res){
+	switch (req.method){
+		case 'POST':
+			var item = '';
+			req.setEncoding('utf8');
+			req.on('data', function(chunk){
+				item += chunk;
+			});
+			req.on('end', function(){
+				items.push(item);
+				res.end('OK\n');
+			});
+			break;
+		case 'GET':
+			items.forEach(function(item, i){
+				res.write(i + ') ' + item + '\n');
+			});
+			res.end();
+			break;
+	}
+});
 
-// create router
-var commentRouter = express.Router();
-
-// use body-parser
-commentRouter.use(bodyParser.json());
-
-// configure router
-commentRouter.route('/')
-    .get(function(req, res, next) {
-        Comments.find({}, function(err, comments) {
-            if (err) throw err;
-            res.json(comments);
-        });
-    })
-
-    .post(function(req, res, next) {
-        Comments.create(req.body, function(err, comment) {
-            if (err) throw err;
-            console.log('Comment created!');
-            var id = comment._id;
-            res.writeHead(200, {
-                'Content-Type': 'text/plain'
-            });
-
-            res.end('Added the comment with id: ' + id);
-        });
-    })
-
-    .delete(function(req, res, next) {
-        Comments.remove({}, function(err, resp) {
-            if (err) throw err;
-            res.json(resp);
-        });
-    });
-
-commentRouter.route('/:commentId')
-    .get(function(req, res, next) {
-        Comments.findById(req.params.commentId, function(err, comment) {
-            if (err) throw err;
-            res.json(comment);
-        });
-    })
-
-    .put(function(req, res, next) {
-        Comments.findByIdAndUpdate(req.params.commentId, {
-            $set: req.body
-        }, {
-            new: true
-        }, function(err, comment) {
-            if (err) throw err;
-            res.json(comment);
-        });
-    })
-
-    .delete(function(req, res, next) {
-        Comments.findByIdAndRemove(req.params.commentId, function(err, resp) {
-            if (err) throw err;
-            res.json(resp);
-        });
-    });
-
-// export module
-module.exports = commentRouter;
+server.listen(8080);
